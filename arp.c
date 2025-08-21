3159bb37587b98f030bd0e8ebaa1da1a254f6e3e
egeorgel <egeorgel@student.42nice.fr>
2025-08-21 10:40:25 -0400

    read sender instead of target

--- arp.c
+++ arp.c
@@ -20,18 +20,18 @@ void	print_broadcast(runtime* run, arp_packet *request)
 	if (!if_indextoname(run->interface.sll_ifindex, ifname))
 		err_exit(ERR_MAX, run);
 	print_step(STEP_INTERFACE, run, ifname);
-	print_step(STEP_BROADCAST, run, request->target_mac, request->target_ip);
+	print_step(STEP_BROADCAST, run, request->sender_mac, request->sender_ip);
 }
 
 bool	listen_arp(runtime *run, int sock)
 {
-	arp_packet *request = malloc(sizeof(arp_packet));
+	arp_packet request;
 	socklen_t interface_len = sizeof(run->interface);
-	ssize_t len = recvfrom(sock, request, sizeof(arp_packet), 0, (struct sockaddr*)&run->interface, &interface_len);
+	ssize_t len = recvfrom(sock, &request, sizeof(arp_packet), 0, (struct sockaddr*)&run->interface, &interface_len);
 
-	if (len > 0 && request->arp_header.ar_op == htons(ARPOP_REQUEST))
+	if (len > 0 && request.arp_header.ar_op == htons(ARPOP_REQUEST))
 	{
-		print_broadcast(run, request);
+		print_broadcast(run, &request);
 		return true;
 	}
 	return false;
