7423a54d660bb28c6d899ebc2785a8fa01ef6664
egeorgel <egeorgel@student.42nice.fr>
2025-08-25 09:58:24 -0400

    test at fixing mac parsing

--- parsing.c
+++ parsing.c
@@ -1,6 +1,13 @@
 #include "malcolm.h"
 
-static void	parse_mac(char* src, char* dest, runtime* run, bool has_star)
+static void	hex_to_uchar(unsigned char* dest, char *src)
+{
+	*dest = src[0];
+	*dest <<= 4;
+	*dest &= src[1];
+}
+
+static void	parse_mac(char* src, unsigned char* dest, runtime* run, bool has_star)
 {
 	bool star = false;
 	int j = 0;
@@ -17,8 +24,8 @@ static void	parse_mac(char* src, char* dest, runtime* run, bool has_star)
 			err_exit(INV_MAC, run, src);
 		else if (!is_hexa(&src[j], 2))
 			err_exit(INV_MAC, run, src);
-		ft_strlcpy(&dest[k], &src[j], 2);
-		k += 2;
+		hex_to_uchar(&dest[k], &src[j]);
+		k++;
 		j += 2;
 		if (!(i < 5))
 			break;
@@ -43,7 +50,7 @@ void	parse_params(int argc, char**argv, runtime* run)
 	if (argc != 5)
 		err_exit(INV_PARAMS, run);
 	run->ip_src = parse_ip(argv[1], run);
-	parse_mac(argv[2], (char*)run->mac_src, run, false);
+	parse_mac(argv[2], run->mac_src, run, false);
 	run->ip_trg = parse_ip(argv[3], run);
-	parse_mac(argv[4], (char*)run->mac_trg, run, true);
+	parse_mac(argv[4], run->mac_trg, run, true);
 }
